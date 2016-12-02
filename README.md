ce7eec49a48d90fde6bc77ea7db91165b17e5f23
Adrian P <adrian@ajpennington.net>
2016-12-03 00:03:58 +1100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# godaddy-ddns
+# Godaddy DDNS
 Provides DDNS functionality for GoDaddy managed DNS Domains
+
+## Motivation
+If you like running your own webserver from home using a domain from GoDaddy but you have a dynamic IP address then this
+program could be the solution to your problem.
+
+This Ruby script detects your external IP address and updates A records in your GoDaddy domain for you. You can set this up in a cron or even daemonize it inside a shell script (be very, very careful its not designed for that) for automatic updates.
+
+Before using this you will need to get your GoDaddy API-key and secret.
+
+## API Keys
+1. Navigate to [GoDaddy Developer Portal](https://developer.godaddy.com/) and click on "Keys".
+1. Login with your GoDaddy account.
+1. Under "Production" click on the + sign.
+1. Note down your API-key and secret.
+
+## Requirements
+Tested on Ruby 2.3
+
+## Installation and Configration
+1. Check out this repository
+1. Add the API key and Secret to the godaddy_ddns.yaml file
+1. Add what domain you want to update (currently the script only supports updating one domain at a time)
+
+```yaml
+domain: yourdomain.com
+```
+
+1. Add the A record hostname(s) you want to be dynamically updated each time the script is run.
+
+```yaml
+dns-arecords:
+	- www
+	- mail
+```
+
+## Running
+This script can be run inside a cron or just manually.
+
+During execution the script writes to a file the users current external IP address. The next time the script is run it checks to see if this address has changed, if it has it will update the records with GoDaddy and update the external address file (remote_ip.addr). If the address has not changed it will simply do nothing.
+
+
+If there was a problem with connecting to the API an error will be reported to STDERR handy for capturing errors when run inside a cron.
+
+This script returns 0 on success and 1 on failure which can be handy.
+
+## Warranty
+This is free software and I offer no warranty whatsoever.
---
+++ godaddy_ddns.rb
@@ -0,0 +1,96 @@
+#!/usr/bin/env ruby
+# This script uses the GoDaddy API to update DNS A records 
+# automatically if you dynamic IP changes
+# 
+# Author: LightReign 12/2016
+
+require 'open-uri'
+require 'net/http'
+require 'net/https'
+require 'yaml'
+
+def get_config()
+   return YAML.load_file('godaddy_ddns.yaml') 
+end
+
+
+def get_remote_ip(url)
+    return open(url).read
+end
+
+def read_ipaddr_file(filename)
+    file = open(filename, 'r')
+    old_remote_ip = file.read.chomp
+    file.close
+
+    return old_remote_ip
+end
+
+def write_ipaddr_file(filename, remote_ip)
+    file = open(filename, 'w')
+    file.write(remote_ip)
+    file.close
+end
+
+def update_godaddy(config, remote_ip, domain, record_type, record_name)
+    uri = URI.parse("https://api.godaddy.com/v1/domains/#{domain}/records/#{record_type}/#{record_name}")
+    req = Net::HTTP::Put.new(uri.path)
+
+    # set constants
+    req['Authorization'] = "sso-key #{config['api-key']}:#{config['secret']}"
+
+    json =  <<-eos
+    [
+        {
+            "data": "#{remote_ip}"
+        }
+    ]
+    eos
+
+    req.body = json
+    req.content_type = 'application/json'
+
+    https = Net::HTTP.new(uri.hostname, uri.port)
+    https.use_ssl = true
+
+    res = https.request(req)
+
+    case res
+    when Net::HTTPSuccess, Net::HTTPRedirection
+        return false
+    else
+        STDERR.puts "HTTP Error: #{res.code} #{res.message}: #{res.body}"
+    end
+
+    return true
+end
+
+## Main
+
+ipaddr_filename = 'remote_ip.addr';
+old_remote_ip = nil
+error = false
+
+config = get_config()
+
+abort "ip-checker-url is empty, aborting..." unless config['ip-checker-url']
+
+remote_ip = get_remote_ip(config['ip-checker-url'])
+
+if (File.file?(ipaddr_filename))
+    old_remote_ip = read_ipaddr_file(ipaddr_filename)
+end
+
+
+# if the ip address has changed then update!
+if (old_remote_ip != remote_ip)
+    for record_name in config['dns-arecords']
+        error = update_godaddy(config, remote_ip, config['domain'], 'A', record_name)
+
+        break if error
+    end
+
+    write_ipaddr_file(ipaddr_filename, remote_ip) unless error
+end
+
+exit error ? 1 : 0
